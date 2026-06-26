bdaeeb090b9c9741cbd53f021970cd41a25399e3
Mackenzie Hoyle <mackenzie.hoyle@versaterm.com>
2026-06-26 10:51:27 -0400

    update options.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Python: use basedpyright (installed via Mason) instead of LazyVim's
+-- default pyright, and ruff for linting/formatting. Must be set before the
+-- lang.python extra loads (options.lua loads pre-lazy).
+vim.g.lazyvim_python_lsp = "basedpyright"
+vim.g.lazyvim_python_ruff = "ruff"
